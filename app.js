c3fd4df1f3d2acbe059543684c1f0e654799a046
stealerwolf <davimuza90@gmail.com>
2024-04-13 00:53:42 -0300

    fixed paths

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require("express");
+var path = require("path");
 
 const app = express();
 const port = 3000;
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+  console.log(`running ${port}`);
 });
